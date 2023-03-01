67a0e0cb221c5f1f349ef33d65d10530717c3b32
Mathews Dantas <94187504+MathewsDantas@users.noreply.github.com>
2023-03-01 10:14:22 -0300

    script em pergunta frequentes e galeria de bicicletas.

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 
-//ativar links menu
+//ativar links menu -----------------------------------------------------------------
 const links = document.querySelectorAll('.header-menu a');
 
 console.log(links);
@@ -15,7 +15,7 @@ function ativarLink(item) {
 
 links.forEach(ativarLink);
 
-//ativar links do orcamento
+//ativar links do orcamento -----------------------------------------------------------------
 
 function ativarParametros(item, index) {
     const elemento = document.getElementById(item)
@@ -29,3 +29,39 @@ function ativarParametros(item, index) {
 const parametros = new URLSearchParams(location.search);
 parametros.forEach(ativarParametros)
 console.log(parametros);
+
+//perguntas frequentes. -----------------------------------------------------------------
+
+function ativarPergunta(event) {
+    console.log(event)
+    const pergunta = event.currentTarget
+    const control = pergunta.getAttribute('aria-controls')
+    const resposta = document.getElementById(control)
+    // pergunta.setAttribute('aria-expanded', "true")
+    resposta.classList.toggle('ativa')// toggle -> add e remove 
+    console.log(resposta)
+}
+
+function eventosPergunta(item) {
+    item.addEventListener('click', ativarPergunta)
+    console.log(item)
+}
+
+const perguntas = document.querySelectorAll('.perguntas button')
+perguntas.forEach(eventosPergunta)
+
+// Galeria de bicicletas -----------------------------------------------------------------
+
+const galeria = document.querySelectorAll('.bicicleta-imagens img')
+const galeriaContainer = document.querySelector('.bicicleta-imagens')
+
+function trocarImagem(event) {
+    const img = event.currentTarget
+    const media = window.matchMedia('(min-width: 800px)').matches
+    if (media) galeriaContainer.prepend(img)//remove o elemento e coloca-o na frente
+    console.log(galeriaContainer)
+}
+
+galeria.forEach((item) => {
+    item.addEventListener('click', trocarImagem)
+})
